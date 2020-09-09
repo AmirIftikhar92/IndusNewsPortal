45b333624f4e618cb3771b98f65f2d87226306bd
Amir Iftikhar <amir_2824@hotmail.com>
2020-09-10 04:01:40 +0500

    .

---
+++ IndusNews_PortalApp/Controllers/JobApplicantsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IndusNews_PortalApp.Models;
+
+namespace IndusNews_PortalApp.Controllers
+{
+    public class JobApplicantsController : Controller
+    {
+        private DB_A66B9F_dbindusportalEntities db = new DB_A66B9F_dbindusportalEntities();
+        // GET: JobApplicants
+        [Authorize (Users ="hr@indus.news")]
+        public ActionResult Index()
+        {
+            var jobApplicants = db.JobApplicants.Include(j => j.Job);
+            return View(jobApplicants.ToList());
+        }
+
+        // GET: JobApplicants/Details/5
+        [Authorize(Users = "hr@indus.news")]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            JobApplicant jobApplicant = db.JobApplicants.Find(id);
+            if (jobApplicant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jobApplicant);
+        }
+
+        // GET: JobApplicants/Create
+        public ActionResult Create()
+        {
+            ViewBag.AppliedJob = new SelectList(db.Jobs, "JobId", "JobTitle");
+            return View();
+        }
+
+        // POST: JobApplicants/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ApplicantId,ApplicantName,ApplicantEmail,ApplicantPhone,AppliedJob,ApplyDate")] JobApplicant jobApplicant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.JobApplicants.Add(jobApplicant);
+                db.SaveChanges();
+                return RedirectToAction("Careers","Home");
+            }
+
+            ViewBag.AppliedJob = new SelectList(db.Jobs, "JobId", "JobTitle", jobApplicant.AppliedJob);
+            return View(jobApplicant);
+        }
+
+        // GET: JobApplicants/Edit/5
+        [Authorize(Users = "hr@indus.news")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            JobApplicant jobApplicant = db.JobApplicants.Find(id);
+            if (jobApplicant == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AppliedJob = new SelectList(db.Jobs, "JobId", "JobTitle", jobApplicant.AppliedJob);
+            return View(jobApplicant);
+        }
+
+        // POST: JobApplicants/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Users = "hr@indus.news")]
+        public ActionResult Edit([Bind(Include = "ApplicantId,ApplicantName,ApplicantEmail,ApplicantPhone,AppliedJob,ApplyDate")] JobApplicant jobApplicant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(jobApplicant).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AppliedJob = new SelectList(db.Jobs, "JobId", "JobTitle", jobApplicant.AppliedJob);
+            return View(jobApplicant);
+        }
+
+        // GET: JobApplicants/Delete/5
+        [Authorize(Users = "hr@indus.news")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            JobApplicant jobApplicant = db.JobApplicants.Find(id);
+            if (jobApplicant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jobApplicant);
+        }
+
+        // POST: JobApplicants/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Users = "hr@indus.news")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            JobApplicant jobApplicant = db.JobApplicants.Find(id);
+            db.JobApplicants.Remove(jobApplicant);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- IndusNews_PortalApp/Models/JobApplyViewModel.cs
+++ IndusNews_PortalApp/Models/JobApplyViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,18 +8,20 @@ namespace IndusNews_PortalApp.Models
 {
     public class JobApplyViewModel
     {
+        [Key]
         public int ApplicantId { get; set; }
         public string ApplicantName { get; set; }
         public string ApplicantEmail { get; set; }
         public string ApplicantPhone { get; set; }
+        public Nullable<int> AppliedJob { get; set; }
+        public Nullable<System.DateTime> ApplyDate { get; set; }
         public int CVId { get; set; }
         public string CVFileName { get; set; }
         public string CVContentType { get; set; }
         public byte[] CVFile { get; set; }
-        public Nullable<int> AppliedJob { get; set; }
-        public Nullable<System.DateTime> ApplyDate { get; set; }
+        public virtual JobApplicant JobApplicant { get; set; }
+        public virtual ICollection<JobApplicant> JobApplicants { get; set; }
         public virtual ICollection<CV> CVs { get; set; }
         public virtual Job Job { get; set; }
-        public virtual JobApplicant JobApplicant { get; set; }
     }
 }
